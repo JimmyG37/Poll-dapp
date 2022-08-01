afa56ba82ac8a842778907e87cab47e24b8ca187
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-08-01 14:59:41 -0700

    Method for a candidate to register for an election

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -5,29 +5,25 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 error Voting__AdjustRegistrationPeriod();
 error Voting__AdjustVotingPeriod();
+error Voting__AlreadyRegistered();
+error Voting__RegistrationPeriodOver();
 
 contract Voting {
     using Counters for Counters.Counter;
-    enum Registration {
-        OPEN,
-        CLOSED
-    }
 
-    enum Vote {
-        OPEN,
-        CLOSED
+    enum Registered {
+        NO,
+        YES
     }
     struct Election {
         string title;
         uint256 registrationPeriod;
         uint256 votingPeriod;
-        Registration registrationState;
-        Vote voteState;
     }
 
     struct Candidate {
         string name;
-        string description;
+        Registered isRegistered;
         uint256 voteCount;
     }
 
@@ -38,15 +34,16 @@ contract Voting {
 
     Counters.Counter private s_electionIds;
     mapping(uint256 => Election) private s_elections;
-    mapping(address => Candidate) private s_candidates;
+    mapping(address => mapping(uint256 => Candidate)) private s_candidates;
     mapping(address => uint256[]) private s_voterToElectionId;
 
-    /* Events */
     event ElectionCreated(
         uint256 indexed electionId,
         uint256 indexed registrationPeriod,
         uint256 indexed votingPeriod
     );
+
+    event CandidateResgistered(address indexed candidate, uint256 indexed electionId);
 
     modifier timeCheck(uint256 registrationPeriod, uint256 votingPeriod) {
         if (registrationPeriod <= block.timestamp) {
@@ -58,6 +55,22 @@ contract Voting {
         _;
     }
 
+    modifier _isRegistered(address candidate, uint256 electionId) {
+        Candidate memory _candidate = s_candidates[candidate][electionId];
+        if (_candidate.isRegistered == Registered.YES) {
+            revert Voting__AlreadyRegistered();
+        }
+        _;
+    }
+
+    modifier registerDeadLine(uint256 electionId) {
+        Election memory election = s_elections[electionId];
+        if (block.timestamp >= election.registrationPeriod) {
+            revert Voting__RegistrationPeriodOver();
+        }
+        _;
+    }
+
     function createElection(
         string memory _title,
         uint256 _registrationPeriod,
@@ -65,14 +78,25 @@ contract Voting {
     ) public timeCheck(_registrationPeriod, _votingPeriod) {
         s_electionIds.increment();
         uint256 newElectionId = s_electionIds.current();
-        s_elections[newElectionId] = Election(
-            _title,
-            _registrationPeriod,
-            _votingPeriod,
-            Registration.OPEN,
-            Vote.OPEN
-        );
+        s_elections[newElectionId] = Election(_title, _registrationPeriod, _votingPeriod);
         emit ElectionCreated(newElectionId, _registrationPeriod, _votingPeriod);
+    }
+
+    function candidateRegistration(uint256 electionId, string memory _name)
+        external
+        _isRegistered(msg.sender, electionId)
+        registerDeadLine(electionId)
+    {
+        s_candidates[msg.sender][electionId] = Candidate(_name, Registered.YES, 0);
+        emit CandidateResgistered(msg.sender, electionId);
+    }
+
+    function getCandidate(address candidate, uint256 electionId)
+        external
+        view
+        returns (Candidate memory)
+    {
+        return s_candidates[candidate][electionId];
     }
 
     function getElection(uint256 electionId) external view returns (Election memory) {
