09e4d5457e334d077fabfcc78cef6053412c6dde
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-08-07 12:07:56 -0700

    Sidebar import

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 import Header from "../components/Header"
+import Sidebar from "../components/Sidebar"
 
 function MyApp({ Component, pageProps }) {
     return (
         <MoralisProvider initializeOnMount={false}>
             <Header />
+            <Sidebar />
             <Component {...pageProps} />
         </MoralisProvider>
     )
