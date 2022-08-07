15dd3f8fbe5c3551abf4ae5482d12ab8a0fcb7d6
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-08-07 14:46:55 -0700

    Apollo

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,21 @@ import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 import Header from "../components/Header"
 import Sidebar from "../components/Sidebar"
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+
+const client = new ApolloClient({
+    uri: "https://api.studio.thegraph.com/query/31431/postchain/v0.01",
+    cache: new InMemoryCache(),
+})
 
 function MyApp({ Component, pageProps }) {
     return (
         <MoralisProvider initializeOnMount={false}>
-            <Header />
-            <Sidebar />
-            <Component {...pageProps} />
+            <ApolloProvider client={client}>
+                <Header />
+                <Sidebar />
+                <Component {...pageProps} />
+            </ApolloProvider>
         </MoralisProvider>
     )
 }
