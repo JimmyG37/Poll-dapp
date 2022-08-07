2bdd1940500d9175875524234ce41ab9b76347fb
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-08-07 14:46:27 -0700

    Post, Comment, Like queries

---
+++ constants/subgraphQueries.js
@@ -0,0 +1,33 @@
+import { gql } from "@apollo/client"
+
+const GET_POSTS = gql`
+    {
+        posts(first: 5) {
+            id
+            creator
+            postId
+            likeDeadline
+        }
+    }
+`
+const GET_COMMENTS = gql`
+    {
+        comments(first: 5) {
+            id
+            commenter
+            postId
+            commentId
+        }
+    }
+`
+
+const GET_LIKES = gql`
+    {
+        likes(first: 5) {
+            user
+            postId
+            commentId
+        }
+    }
+`
+export { GET_POSTS, GET_COMMENTS, GET_LIKES }
