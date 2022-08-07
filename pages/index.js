16434fe9279ab8576e1a7e09b0a46556c76fe716
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-08-07 14:47:21 -0700

    Feed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
+import Feed from "../components/Feed"
 
 export default function Home() {
-    return <div>Hello</div>
+    return (
+        <div>
+            <Feed />
+        </div>
+    )
 }
