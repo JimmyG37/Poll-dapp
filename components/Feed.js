af2534774fa637822dc1eaeda92b250e1b50acdb
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-08-07 14:45:50 -0700

    Feed component

---
+++ components/Feed.js
@@ -0,0 +1,7 @@
+export default function Feed() {
+    return (
+        <div className="flex-grow border-l border-r border-slate-50 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
+            <div className="pb-72">Hello From Feed</div>
+        </div>
+    )
+}
