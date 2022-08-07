ccb6c976b153231edb1a2801c7fbcd31544133d7
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-08-07 12:06:12 -0700

    Title position

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,8 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
-            <h1 className="py-4 px-4 font-bold text-3xl">PostChain</h1>
+        <nav className="p-4 border-b-2 flex flex-row justify-between items-center">
+            <h1 className="py-4 pl-12 font-bold text-3xl">PostChain</h1>
             <div className="flex flex-row items-center">
                 <Link href="/">
                     <a className="mr-4 p-6">Home</a>
