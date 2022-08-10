26a7aaac3ee6816d6b54ca7de115df233101cdc8
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-08-10 12:50:02 -0700

    Added header and sidebar

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,18 @@ import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
 import Feed from "../components/Feed"
+import Header from "../components/Header"
+
+import Sidebar from "../components/Sidebar"
 
 export default function Home() {
     return (
-        <div className=" min-h-screen flex max-w-[1500px] mx-auto">
-            <Feed />
+        <div>
+            <Header />
+            <div className=" min-h-screen flex max-w-[1500px] mx-auto">
+                <Sidebar />
+                <Feed />
+            </div>
         </div>
     )
 }
