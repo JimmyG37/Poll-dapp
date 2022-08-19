074e0c8e4ed7d5bcfae64c29ed264221042fc269
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-08-18 17:01:12 -0700

    Polygonscan verification

--- deploy/01-deploy-post-chain.js
+++ deploy/01-deploy-post-chain.js
@@ -1,16 +1,12 @@
 const { network } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
-const fs = require("fs")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
 
-    const smileSVG = await fs.readFileSync("./images/smile.svg", { encoding: "utf8" })
-    const glassesSVG = await fs.readFileSync("./images/glasses.svg", { encoding: "utf8" })
-    const sunglassesSVG = await fs.readFileSync("./images/sunglasses.svg", { encoding: "utf8" })
-    const args = [smileSVG, glassesSVG, sunglassesSVG]
+    const args = []
     const postChain = await deploy("PostChain", {
         from: deployer,
         args: args,
@@ -18,11 +14,11 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         waitConfirmations: network.config.blockConfirmations || 1,
     })
 
-    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    if (!developmentChains.includes(network.name) && process.env.POLYGONSCAN_API_KEY) {
         log("Verifying...")
         await verify(postChain.address, args)
     }
     log("--------------------------------------")
 }
 
-module.exports.tags = ["all", "postChain"]
+module.exports.tags = ["all", "postchain"]
