57403ceeaa491e63c2cf58cdfb385d1c33cb789b
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-08-31 17:29:47 -0700

    combined deadlines

--- scripts/create-post.js
+++ scripts/create-post.js
@@ -1,14 +1,11 @@
 const { ethers, network } = require("hardhat")
 
 async function createPost() {
-    let dateInAWeek = new Date() // now
-    let dateInAWeek2 = new Date()
-    dateInAWeek.setDate(dateInAWeek.getDate() + 1) // add 1 days
-    dateInAWeek2.setDate(dateInAWeek2.getDate() + 2) // add 2 days
-    let commentDeadline = Math.floor(dateInAWeek.getTime() / 1000) // unix timestamp
-    let likeDeadline = Math.floor(dateInAWeek2.getTime() / 1000)
+    let dateInAWeek = new Date()
+    dateInAWeek.setDate(dateInAWeek.getDate() + 7)
+    let deadline = Math.floor(dateInAWeek.getTime() / 1000)
     const postChain = await ethers.getContract("PostChain")
-    const postChainTx = await postChain.createPost("Hello World", commentDeadline, likeDeadline)
+    const postChainTx = await postChain.createPost("Hello World", deadline)
     const postChainTxReceipt = await postChainTx.wait(1)
     const postId = postChainTxReceipt.events[0].args.postId
     console.log(`Post created! ${postId.toString()}`)
