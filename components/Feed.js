c87d9932c2eb630aca8e5c341cd7c3fac24f645f
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-09-06 21:32:06 -0700

    useEffect

--- components/Feed.js
+++ components/Feed.js
@@ -3,6 +3,7 @@ import GET_POSTS from "../constants/queryPosts"
 import CreatePost from "./CreatePost"
 import { useQuery } from "@apollo/client"
 import { SparklesIcon } from "@heroicons/react/24/outline"
+import { useEffect, useState } from "react"
 import { useMoralis, useWeb3Contract } from "react-moralis"
 import networkMapping from "../constants/networkMapping.json"
 import PostChain from "../artifacts/contracts/PostChain.sol/PostChain.json"
@@ -12,6 +13,9 @@ export default function Feed() {
     const chainString = chainId ? parseInt(chainId).toString() : "31337"
     const postChainAddress = networkMapping[chainString].PostChain[0]
     const { loading, error, data: createdPosts } = useQuery(GET_POSTS)
+
+    useEffect(() => {}, [isWeb3Enabled, account, createdPosts])
+
     return (
         <div className="flex-grow border-l border-r border-gray-200 max-w-2xl ">
             <div className="text-black flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 border-gray-200 ">
@@ -28,7 +32,12 @@ export default function Feed() {
                         <CreatePost />
                         <div className="pb-72">
                             {createdPosts.posts.map((post) => {
-                                return <Post id={post.postId} key={`${post.id}${post.postId}`} />
+                                return (
+                                    <Post
+                                        postId={parseInt(post.postId)}
+                                        key={`${post.id}${post.postId}`}
+                                    />
+                                )
                             })}
                         </div>
                     </div>
