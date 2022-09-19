55b650b7056e92ffbe36b4c0896f30585e914f6a
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-09-19 11:00:57 -0700

    flowbite header

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,22 @@
 import { ConnectButton } from "web3uikit"
+import { Navbar } from "flowbite-react"
+import Logo from "./Logo"
 import Link from "next/link"
 
 export default function Header() {
     return (
-        <div className="p-4 border-b-2 flex flex-row justify-between items-center border-gray-100">
-            <h1 className="py-4 pl-12 font-bold text-3xl">PostChain</h1>
-            <div className="flex flex-row items-center">
-                <Link href="/">
-                    <a className="mr-4 p-6">Home</a>
-                </Link>
+        <div className="py-2 border-b-2 border-gray-100">
+            <Navbar>
+                <Navbar.Brand>
+                    <Logo />
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white ml-5">
+                        PostChain
+                    </span>
+                </Navbar.Brand>
+
+                <Navbar.Toggle />
                 <ConnectButton moralisAuth={false} />
-            </div>
+            </Navbar>
         </div>
     )
 }
