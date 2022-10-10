ebd7046d2a07653a3f54407fbf07393d383d50e7
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-10-09 17:28:56 -0700

    post context, brought in tip amount hook

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,27 @@ import Feed from "../components/Feed"
 import Balances from "../components/Balances"
 import Sidebar from "../components/Sidebar"
 import { useMoralis, useWeb3Contract } from "react-moralis"
-import { useEffect, useState } from "react"
-import { ConnectButton } from "@web3uikit/web3"
+import { useEffect, useState, useMemo } from "react"
+import { PostContext } from "../hooks/PostContext"
+import CommentSection from "../components/CommentSection"
+import { useTipAmount } from "../hooks/useTipAmount"
 
 export default function Home() {
+    const [post, setPost] = useState(null)
+    const value = useMemo(() => ({ post, setPost }), [post, setPost])
+    const tipAmount = useTipAmount()
+
+    useEffect(() => {}, [])
+
     return (
-        <div>
-            <div className="screen">
-                <Sidebar />
-                <Feed />
-                <Balances />
-            </div>
-        </div>
+        <>
+            <PostContext.Provider value={value}>
+                <div className="screen">
+                    <Sidebar />
+                    <Feed tipAmount={tipAmount} />
+                    <Balances />
+                </div>
+            </PostContext.Provider>
+        </>
     )
 }
