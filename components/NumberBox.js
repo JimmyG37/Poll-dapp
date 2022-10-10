e8e341a719df086345b86cd548a978b7abb44e14
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-10-09 17:22:15 -0700

    size change, color change

--- components/NumberBox.js
+++ components/NumberBox.js
@@ -4,15 +4,19 @@ export const NumberBox = ({ num, unit, flip }) => {
     return (
         <div className="flex flex-col items-center px-2 pb-4">
             <div className=" relative bg-transparent flex flex-col items-center justify-center rounded-lg text-sm md:text-sm mt-4 ">
-                <div className="rounded-t-lg rounded-b-lg bg-[#1d9bf0] w-8 h-8 "></div>
-                <div className="text-sm absolute text-white z-10 font-bold  md:text-sm">{num}</div>
+                <div className="rounded-t-[4px] rounded-b-[4px] bg-[#333333] w-8 h-8 "></div>
+                <div className="text-sm absolute text-rose-500 z-10 font-bold  md:text-sm">
+                    {num}
+                </div>
                 <div
-                    className={`absolute  w-full h-1/2 top-0  rounded-t-lg z-5 ${
-                        flip ? "animate-flip bg-[#177cc0]" : "bg-transparent"
+                    className={`absolute  w-full h-1/2 top-0  rounded-t-[4px] z-5 ${
+                        flip ? "animate-flip bg-[#404040]" : "bg-transparent"
                     }`}
                 ></div>
+                <div className="absolute right-[-3px] top-[14px] rounded-full w-[5px] h-[5px] bg-[#252526]"></div>
+                <div className="absolute left-[-3px] top-[14px] rounded-full w-[5px] h-[5px] bg-[#252526]"></div>
             </div>
-            {/* <p className="text-sm font-semibold text-[#6e767d]">{unit}</p> */}
+            <p className="text-sm sm:text-[7px] font-bold text-[#6e767d]">{unit}</p>
         </div>
     )
 }
