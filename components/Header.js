bb27bfc8c7de708c7ed35d8b8b17ae07075cb04f
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-10-09 17:21:20 -0700

    color change

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        <nav className="flex h-30 justify-between w-full bg-white border-gray-200 px-5 lg:px-6 - dark:bg-slate-50 z-50 fixed mb-5">
+        <nav className="flex h-30 justify-between w-full bg-[#1e1e1e] border-[#373737] px-5 lg:px-6  z-50 fixed mb-5">
             <div className="flex order-first">
                 <div className="flex items-center">
                     {/* <Logo /> */}
