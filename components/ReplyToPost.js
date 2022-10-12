a59bd3c4d7fbd22318bfd6e2141055b09eedb863
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-10-12 11:03:41 -0700

    condensed styling

--- components/ReplyToPost.js
+++ components/ReplyToPost.js
@@ -60,12 +60,12 @@ export default function ReplyToPost({ postId }) {
                             onChange={(e) => setCommentText(e.target.value)}
                             placeholder="Reply to post"
                             rows="2"
-                            className="bg-transparent outline-none text-white text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px] pt-1.5"
+                            className="widgetInput"
                         />
 
-                        <div className="pt-1">
+                        <div className="pl-3 pt-1">
                             <button
-                                className="bg-blue-700 text-white rounded-full w-20 h-11 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+                                className="widgetButton"
                                 type="submit"
                                 onClick={(e) => handleComment(e)}
                             >
