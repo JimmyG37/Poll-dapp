82492f5f0d72c1928bda173447f5a687d3f73bbd
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-10-17 21:44:48 -0700

    removed routes

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,34 +2,16 @@ import { TabList, Button } from "@web3uikit/web3"
 import Link from "next/link"
 
 export default function Sidebar() {
-    const menuItems = [
-        {
-            href: "/",
-            title: "Home",
-        },
-        {
-            href: "/profile",
-            title: "Profile",
-        },
-        {
-            href: "/market",
-            title: "Market",
-        },
-    ]
-
     return (
         <div className="sidebar">
             <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
-                {menuItems.map(({ href, title }) => (
-                    <div
-                        className={`text-black flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation`}
-                        key={`${href}${title}`}
-                    >
-                        <Link href={href}>
-                            <a>{title}</a>
-                        </Link>
-                    </div>
-                ))}
+                <div
+                    className={`text-black flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation`}
+                >
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                </div>
             </div>
         </div>
     )
