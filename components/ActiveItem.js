365c4ee75ae85e195603a7cb2b82bbda82106f95
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-10-20 23:09:20 -0700

    re-query update, data check

--- components/ActiveItem.js
+++ components/ActiveItem.js
@@ -1,42 +1,47 @@
-import { useEffect, useState, useContext } from "react"
-import { useQuery, useLazyQuery } from "@apollo/client"
-import { gql } from "@apollo/client"
-import ListedNft from "./ListedNft"
+import React, { useEffect, useState } from "react"
+import { useMoralis, useWeb3Contract } from "react-moralis"
+import { ListedNft } from "./ListedNft"
+import GET_ACTIVE_ITEMs from "../constants/queryActiveItems"
+import { useQuery } from "@apollo/client"
 
-const GET_ACTIVE_ITEMs = gql`
-    {
-        activeItems(where: { buyer: "0x0000000000000000000000000000000000000000" }) {
-            id
-            buyer
-            seller
-            nftAddress
-            postId
-            price
+export const ActiveItem = ({ tokenId, imageURI }) => {
+    const { isWeb3Enabled, user } = useMoralis()
+    const { loading, error, data, startPolling, stopPolling } = useQuery(GET_ACTIVE_ITEMs)
+
+    useEffect(() => {}, [isWeb3Enabled, tokenId, imageURI])
+
+    useEffect(() => {
+        startPolling(2000)
+        return () => {
+            stopPolling()
         }
-    }
-`
-
-export default function ActiveItem({ tokenId, imageURI }) {
-    const { loading, error, data } = useQuery(GET_ACTIVE_ITEMs)
-
-    if (loading) return null
-    if (error) return `Error! ${error}`
+    }, [startPolling, stopPolling])
 
     return (
         <>
-            {data.activeItems.map((nft) => {
-                const { price, nftAddress, postId, seller } = nft
-                if (tokenId == postId)
-                    return (
-                        <ListedNft
-                            price={price}
-                            postId={postId}
-                            seller={seller}
-                            imageURI={imageURI}
-                            key={`${nftAddress}${postId}`}
-                        />
-                    )
-            })}
+            {isWeb3Enabled && imageURI ? (
+                loading || !data ? (
+                    <div>Loading...</div>
+                ) : (
+                    <>
+                        {data.activeItems.map((nft) => {
+                            if (tokenId == nft.postId) {
+                                return (
+                                    <ListedNft
+                                        key={`${tokenId}${imageURI}`}
+                                        price={nft.price}
+                                        postId={nft.postId}
+                                        seller={nft.seller}
+                                        imageURI={imageURI}
+                                    />
+                                )
+                            }
+                        })}
+                    </>
+                )
+            ) : (
+                <div>Web3 Not Enabled</div>
+            )}
         </>
     )
 }
