646d6b21c1ceaf4cef36212f139974c5f79cc401
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-10-20 23:26:11 -0700

    added useEffect, isMinted prop

--- hooks/useTokenURI.js
+++ hooks/useTokenURI.js
@@ -2,48 +2,48 @@ import { useEffect, useState } from "react"
 import { useMoralis, useWeb3Contract } from "react-moralis"
 import networkMapping from "../constants/networkMapping.json"
 import PostChainNft from "../artifacts/contracts/PostChainNft.sol/PostChainNft.json"
-import { useMintStatus } from "./useMintStatus"
 
-const useTokenURI = (postId) => {
-    const { chainId, account, isWeb3Enabled } = useMoralis()
+const useTokenURI = (postId, isMinted) => {
+    const { chainId, isWeb3Enabled } = useMoralis()
     const chainString = chainId ? parseInt(chainId).toString() : "31337"
     const postChainNftAddress = networkMapping[chainString].PostChainNft[0]
     const postChainNftAbi = PostChainNft.abi
-    const isMinted = useMintStatus(postId)
     const { runContractFunction } = useWeb3Contract()
     const [imageURI, setImageURI] = useState("")
-    const [tokenName, setTokenName] = useState("")
-    const [tokenDescription, setTokenDescription] = useState("")
 
-    const { runContractFunction: getTokenURI } = useWeb3Contract({
-        abi: postChainNftAbi,
-        contractAddress: postChainNftAddress,
-        functionName: "tokenURI",
-        params: {
-            tokenId: postId,
-        },
-    })
+    const handleTokenURI = async (postId) => {
+        const tokenURI = await runContractFunction({
+            params: {
+                abi: postChainNftAbi,
+                contractAddress: postChainNftAddress,
+                functionName: "tokenURI",
+                params: {
+                    tokenId: postId,
+                },
+            },
+            onError: (error) => {
+                console.log("useTokenURI.js -- tokenURI:", error)
+            },
+        })
 
-    const handleTokenURI = async () => {
-        const tokenURI = await getTokenURI()
         if (tokenURI) {
             const tokenData = tokenURI.replace("data:application/json;base64,", "")
             const decodedTokenData = atob(tokenData)
             const jsonManifest = JSON.parse(decodedTokenData)
             const { name, description, image } = jsonManifest
             setImageURI(image)
-            setTokenName(name)
-            setTokenDescription(description)
         }
     }
 
     useEffect(() => {
         if (isWeb3Enabled && isMinted) {
-            handleTokenURI()
+            handleTokenURI(postId)
         }
-    }, [isWeb3Enabled, postId, isMinted, imageURI, tokenName, tokenDescription])
+    }, [isWeb3Enabled, postId, isMinted])
 
-    return [imageURI, tokenName, tokenDescription]
+    useEffect(() => {}, [setImageURI])
+
+    return imageURI
 }
 
 export { useTokenURI }
