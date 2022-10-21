704301821622fc0b2f6bb2b45fedf04b20e4fc38
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-10-20 23:22:17 -0700

    query activeItems

---
+++ constants/queryActiveItems.js
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client"
+
+const GET_ACTIVE_ITEMs = gql`
+    {
+        activeItems(where: { buyer: "0x0000000000000000000000000000000000000000" }) {
+            id
+            buyer
+            seller
+            nftAddress
+            postId
+            price
+        }
+    }
+`
+
+export default GET_ACTIVE_ITEMs
